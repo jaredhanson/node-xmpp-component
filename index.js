bcdef0292b811307a2e69f7de82a3e8154712d1c
Jared Hanson <jaredhanson@gmail.com>
2015-12-03 10:56:32 -0800

    Add S2XComponent.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict'
 
 var Component = require('./lib/Component')
+var S2XComponent = require('./lib/S2XComponent')
 var core = require('node-xmpp-core')
 
 module.exports = Component
 module.exports.Component = Component
+module.exports.S2XComponent = S2XComponent
 
 core.exportCoreUtils(module.exports)
